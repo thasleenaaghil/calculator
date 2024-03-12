39aa77fc1de3f191d9055485abaf256736dd065a
thasleenaaghil <thasleenalatheefaliyam@gmail.com>
2024-03-12 16:19:34 +0530

    cal

---
+++ cal.js
@@ -0,0 +1,25 @@
+function display(num){
+    result.value+=num
+}
+
+function clearAll(){
+    result.value=""
+}
+function back(){
+    result.value = result.value.slice(0,-1)
+}
+function equal(){
+    result.value =eval(result.value)
+}
+
+function equal(){
+    try{
+        result.value = eval(result.value)
+    }
+    catch{
+        result.value = "error"
+        setTimeout(()=>{
+            result.value=""
+        },1000)
+    }
+}
